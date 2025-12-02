b8c3add9086a6419006e3b785a63e90615ac5ba6
aiskogdata <166698366+aiskogdata@users.noreply.github.com>
2025-12-02 11:50:47 +0100

    init commit

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+
+const axios = require("axios")
+const _ = require("lodash")
+
+async function main() {
+  const nums = [1, 2, 3, 4]
+  console.log("Shuffled:", _.shuffle(nums))
+
+  const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+  console.log("API data:", res.data)
+}
+
+main()
